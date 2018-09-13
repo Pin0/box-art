7cab3b4fb7b7e48a4b3fefeb02b28c0727b950d4
M.Nijdam <m.nijdam@picturae.nl>
2018-09-13 22:10:11 +0200

    fixed removal of colors, some modal changes, tooltips added

--- src/App.vue
+++ src/App.vue
@@ -1,233 +1,256 @@
 <template>
-  <div id="app">
-    <div class="container">
-      <div class="row">
-        <div class="col-4">
-          <b-alert show>BOX ART</b-alert>
-          <input type="range" v-model="boxMargin" min="0" max="20" /> {{ boxMargin }} <br>
-          <input type="range" v-model="numberOfBoxes.width" min="2" max="100" @change="draw"/> {{ numberOfBoxes.width }} <br>
-          <input type="range" v-model="numberOfBoxes.height" min="2" max="100" @change="draw"/> {{ numberOfBoxes.height }} <br>
+    <div id="app">
+        <div class="container">
+            <div class="row">
+                <div class="col-4">
+                    <b-alert show>BOX ART</b-alert>
+                    <input type="range" v-model="boxMargin" min="0" max="20" v-b-tooltip.hover title="Margin"/> {{
+                    boxMargin }} <br>
+                    <input type="range" v-model="numberOfBoxes.width" min="2" max="100" @change="draw" v-b-tooltip.hover
+                           title="Width"/> {{ numberOfBoxes.width }} <br>
+                    <input type="range" v-model="numberOfBoxes.height" min="2" max="100" @change="draw"
+                           v-b-tooltip.hover title="Height"/> {{ numberOfBoxes.height }} <br>
 
-          <div class="clear">
-          <ul id="boxcolors" class="clearfix">
-            <li v-for="boxColor in boxColors" :style="'background-color:' + boxColor +';'">
-            </li>
-          </ul>
-          </div>
-          <div class="clear">
-            <button @click="showModal()">Add Box</button>
-            <button @click="removeBox()">X</button>
-            <button @click="print()">Print</button>
-          </div>
+                    <div class="clear">
+                        <ul id="boxcolors" class="clearfix">
+                            <li v-for="boxColor in boxColors" :style="'background-color:' + boxColor +';'">
+                                <span v-on:click="removeColorBox(boxColor)">X</span>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="clear">
+                        <button @click="showColorModal()">Add Color</button>
+                        <button @click="print()">Print</button>
+                    </div>
+                </div>
+                <div class="col-8">
+                    <div class="clearfix" ref="printMe" id="back" :style="'background-color:' + backColor + ';padding: ' + boxMargin + 'px;'">
+                        <ul id="box">
+                            <li :class="box.style" v-for="box in boxes"
+                                :style="'width: calc(' + boxPercentage + '% - ' + (boxMargin*2) + 'px); padding-bottom: calc(' + boxPercentage + '% - ' + (boxMargin*2) + 'px); background-color:' + box.color +'; margin:' + boxMargin + 'px;'">
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="col-8">
-           <div class="clearfix" ref="printMe" id="back" :style="'background-color:' + backColor + ';'">
-          <ul id="box">
-            <li :class="box.style" v-for="box in boxes" :style="'width: calc(' + boxPercentage + '% - ' + (boxMargin*2) + 'px); padding-bottom: calc(' + boxPercentage + '% - ' + (boxMargin*2) + 'px); background-color:' + box.color +'; margin:' + boxMargin + 'px;'">
-            </li>
-          </ul>
-          </div>
-        </div>
-      </div>
+        <b-modal v-model="colorModalShow" title="Add color" ok-title="Add color" @ok="addBox()">
+            <swatches v-model="colors" @change-color="onChange"></swatches>
+        </b-modal>
+        <b-modal v-model="printModal" size="lg" title="Print" ok-only>
+            <img class="printimage" :src="output">
+        </b-modal>
     </div>
-    <img :src="output">
-    <b-modal v-model="modalShow" @hidden="addBox()">
-      <swatches v-model="colors" @change-color="onChange"></swatches>
-    </b-modal>
-  </div>
 </template>
 
 
-
 <script>
-    import { Swatches } from 'vue-color'
+    import {Swatches} from 'vue-color'
     import bModal from 'bootstrap-vue/es/components/modal/modal'
 
 
     export default {
-  name: 'app',
-    components: {
-        Swatches,
-        'b-modal': bModal
-    },
-  data () {
-    return {
-      output: null,
-      modalShow: false,
-      numberOfBoxes: {
-          height: 10,
-          width: 10
-      },
-      colors: {
-          hex: '#ffffff',
-      },
-      boxColors:['black','white','gray'],
-      boxPercentage: 10,
-      backColor: '#ddd',
-      boxMargin: '0',
-      boxes: [
-      ]
-    }
-  },
-
-    methods: {
-        addBox(){
-            //is this the correct way, this works otherwise I got some __ob__ Observable
-            this.$set(this.boxColors,this.boxColors.length,this.colors.hex);
-            this.draw();
+        name: 'app',
+        components: {
+            Swatches,
+            'b-modal': bModal
         },
-        removeBox(){
-            if(this.boxColors.length == 1) {
-                this.boxColors = [];
-            } else {
-                this.boxColors.splice(-1,1); // why is this removing only the last row?
-
+        data() {
+            return {
+                output: null,
+                colorModalShow: false,
+                printModal: false,
+                numberOfBoxes: {
+                    height: 10,
+                    width: 10
+                },
+                colors: {
+                    hex: '#ffffff',
+                },
+                boxColors: ['black', 'white', 'gray'],
+                //  boxPercentage: 100,
+                backColor: '#ddd',
+                boxMargin: '0',
+                boxes: []
             }
-            this.draw();
         },
-        draw(){
-            this.boxes = [];
-            if(this.boxColors.length == 0) {
-                return;
+        computed: {
+            boxPercentage: function () {
+                return 100 / this.numberOfBoxes.width;
             }
-            for (let boxCount = 0; boxCount < (this.numberOfBoxes.width*this.numberOfBoxes.height); ) {
-                let boxStyle = 'box';
+        },
 
-                for (let i = 0; i < this.boxColors.length; i++) {
+        methods: {
+            addBox() {
+                //is this the correct way, this works otherwise I got some __ob__ Observable
+                this.$set(this.boxColors, this.boxColors.length, this.colors.hex);
+                this.draw();
+            },
 
-                    if ((boxCount % this.numberOfBoxes.width) == 0) {
-                        boxStyle = 'box clear';
-                    } else {
-                        boxStyle = 'box';
-                    }
-                    this.boxes.push({color: this.boxColors[i], style: boxStyle});
-                    boxCount++
-                    if (boxCount == this.numberOfBoxes.width * this.numberOfBoxes.height) {
-                        break;
-                    }
+            removeColorBox: function (index) {
+                this.boxColors.splice(index, 1);
+                this.draw();
+            },
+            draw() {
+                this.boxes = [];
+                if (this.boxColors.length == 0) {
+                    return;
                 }
+                for (let boxCount = 0; boxCount < (this.numberOfBoxes.width * this.numberOfBoxes.height);) {
+                    let boxStyle = 'box';
+
+                    for (let i = 0; i < this.boxColors.length; i++) {
+
+                        if ((boxCount % this.numberOfBoxes.width) == 0) {
+                            boxStyle = 'box clear';
+                        } else {
+                            boxStyle = 'box';
+                        }
+                        this.boxes.push({color: this.boxColors[i], style: boxStyle});
+                        boxCount++
+                        if (boxCount == this.numberOfBoxes.width * this.numberOfBoxes.height) {
+                            break;
+                        }
+                    }
+
+                }
+            },
+            onChange(val) {
+                this.colors = val;
+                this.hideModal();
+            },
+            hideColorModal() {
+                this.colorModalShow = false;
+            },
+            showColorModal() {
+                this.colorModalShow = true;
+            },
+            print() {
+                self = this;
+                this.printModal = true;
+                const el = this.$refs.printMe;
+                // add option type to get the image version
+                // if not provided the promise will return
+                // the canvas.
+                const options = {
+                    type: 'dataURL'
+                }
+                this.$html2canvas(el, options).then(function (result) {
+                    self.output = result;
+                });
 
             }
         },
-        onChange (val) {
-            this.colors = val;
-            this.hideModal();
-        },
-        hideModal () {
-            this.modalShow = false;
-        },
-        showModal () {
-            this.modalShow = true;
-        },
-        print() {
-            self = this;
-            const el = this.$refs.printMe;
-            // add option type to get the image version
-            // if not provided the promise will return
-            // the canvas.
-            const options = {
-                type: 'dataURL'
-            }
-            this.$html2canvas(el, options).then(function(result){
-                self.output = result;
-            });
 
-        }
-    },
+        beforeMount() {
+            this.draw()
+        },
 
-    beforeMount(){
-        this.draw()
-    },
-
-}
+    }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 
-h1, h2 {
-  font-weight: normal;
-}
+    h1, h2 {
+        font-weight: normal;
+    }
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
 
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
+    li {
+        display: inline-block;
+        margin: 0 10px;
+    }
 
-a {
-  color: #42b983;
-}
-.box {
-  margin: 0;
-  float:left;
-}
-#boxcolors {
-  li{
-    float: left;
-    display: inline-block;
-    margin: 0 10px;
-    width: 20px;
-    height: 20px;
-    border: 1px solid #d1ecf1;
-  }
-}
-.clear {
-  clear: both;
-}
-.clearfix:after {
-  content: ".";
-  display: block;
-  clear: both;
-  visibility: hidden;
-  line-height: 0;
-  height: 0;
+    a {
+        color: #42b983;
+    }
 
-}
+    .box {
+        margin: 0;
+        float: left;
+    }
 
-#back {
-  width:100%;
-}
+    #boxcolors {
+        li {
+            float: left;
+            display: inline-block;
+            margin: 0 10px;
+            width: 20px;
+            height: 20px;
+            border: 1px solid #d1ecf1;
 
-.clearfix {
-  display: inline-block;
-}
+            span {
+                font-weight: bold;
+                font-size: 1em;
+                color: white;
+            }
+        }
+    }
 
-html[xmlns] .clearfix {
-  display: block;
-}
+    .clear {
+        clear: both;
+    }
 
-* html .clearfix {
-  height: 1%;
-}
+    .clearfix:after {
+        content: ".";
+        display: block;
+        clear: both;
+        visibility: hidden;
+        line-height: 0;
+        height: 0;
 
-h1 {
-  height: 120px;
-  line-height: 120px;
-  text-align: center;
-}
-.vc-chrome {
-  position: absolute;
-  top: 35px;
-  right: 0;
-  z-index: 9;
-}
-.current-color {
-  display: inline-block;
-  width: 16px;
-  height: 16px;
-  background-color: #000;
-  cursor: pointer;
-}
+    }
+
+    #back {
+        width: 100%;
+    }
+
+    .clearfix {
+        display: inline-block;
+    }
+
+    html[xmlns] .clearfix {
+        display: block;
+    }
+
+    * html .clearfix {
+        height: 1%;
+    }
+
+    h1 {
+        height: 120px;
+        line-height: 120px;
+        text-align: center;
+    }
+
+    .vc-chrome {
+        position: absolute;
+        top: 35px;
+        right: 0;
+        z-index: 9;
+    }
+
+    .current-color {
+        display: inline-block;
+        width: 16px;
+        height: 16px;
+        background-color: #000;
+        cursor: pointer;
+    }
+
+    .printimage {
+        width: 100%;
+    }
 </style>
